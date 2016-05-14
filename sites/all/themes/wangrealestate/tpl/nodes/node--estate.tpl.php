1610b96d4b3aba3590ca5aa61c38d953ecf56047
Eric Chang <szechichang@gmail.com>
2016-05-14 01:52:35 -0400

    test

--- sites/all/themes/wangrealestate/tpl/nodes/node--estate.tpl.php
+++ sites/all/themes/wangrealestate/tpl/nodes/node--estate.tpl.php
@@ -4,7 +4,34 @@
  * Template file for the Estate content type.
  */
 ?>
+<section class="info-description">
+  <div class="title">
+    <h2 class="name">
+      <?php if(!empty($title)): ?>
+       <?php print $title; ?>
+      <?php endif; ?>
+    </h2>
+    
+    <h3 class="address">
+      <?php if(!empty($address)): ?>
+        <?php print $address; ?>
+      <?php endif; ?>
+    </h3>
+  </div>
 
+  <p class="info">List Price: <span></span></p>
+  <p class="info">$/SqFt: <span></span></p>
+  <p class="beds">Bed: <span></span></p>
+  <p class="info">Bath: <span></span></p>
+  <p class="info">SqFt: <span></span></p>
+  <p class="info">Lot Size: <span></span></p>
+  <p class="info">Year Built: <span></span></p>
+  <p class="info">Property Type: <span></span></p>
+  <p class="info">Stories: <span></span></p>
+  <p class="info info-last">MLS#: Brian Wang<span></span></p>
+  <h4 class="description">Description</h4>
+  <p class="description-info">DummyData DummyData DummyData DummyData DummyData DummyData DummyData DummyData DummyData DummyData DummyData DummyData DummyData DummyData</p>
+</section>
 <article class="estate-container">
   <div>
     <?php if (!empty($beds)): ?>
