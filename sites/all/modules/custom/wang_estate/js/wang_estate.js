a4977a05316306305f538699f4c6898ea09260a2
Eric Chang <szechichang@gmail.com>
2016-05-26 17:31:34 -0400

    header footer responsive carousel added or fixed

--- sites/all/modules/custom/wang_estate/js/wang_estate.js
+++ sites/all/modules/custom/wang_estate/js/wang_estate.js
@@ -1,69 +1,29 @@
 (function ($) {
-    $(document).ready(function(){
-        $('.carousel-list').slick({
-            autoplay: true,
-            //Hong's a wizard,
-            dots: true,
-            arrows: false,
-            slidesToShow: 1
-        });
+  $(document).ready(function(){
+    $('.carousel-list').slick({
+      autoplay: true,
+      arrows: false,
+      slidesToShow: 1,
+      focusOnSelect: true,
+      dots: true
     });
+  });
 
-
-    function initialize() {
-var stuff = document.getElementById("googleMap"),
-    ab = stuff.getAttribute('data-lat'),
-    cd = stuff.getAttribute('data-lng'),
-    thisCenter = new google.maps.LatLng(ab,cd);
-      var mapProp = { 
-        center:thisCenter,
-        zoom:15,
-        mapTypeId:google.maps.MapTypeId.ROADMAP
-      };
-      var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-        var marker=new google.maps.Marker ({
-        position:thisCenter
-      });
-        marker.setMap(map);
-    }
-    google.maps.event.addDomListener(window, 'load', initialize);
+  function initialize() {
+    var thisMap = document.getElementById("googleMap"),
+      thisLat = thisMap.getAttribute('data-lat'),
+      thisLon = thisMap.getAttribute('data-lng'),
+    thisCenter = new google.maps.LatLng(thisLat,thisLon);
+    var mapProp = { 
+      center: thisCenter,
+      zoom: 16,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+      var marker = new google.maps.Marker({
+      position: thisCenter
+    });
+      marker.setMap(map);
+  };
+  google.maps.event.addDomListener(window, 'load', initialize);
 })(jQuery);
-
-/*
-    var key = 'AIzaSyBpK9J6q5IWEygekxYrmLbqx-UUOotNJ_A',
-        address = '8324 st james ave elmhurst, ny';
-    var geoUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodeURI(address) + '&key=' + key;
-    console.log(geoUrl);
-
-    $.ajax({
-        url: geoUrl,
-        success: function(data) {
-            //do something
-        }
-    });
-*/
-
-/*
-    var geocoder, map;
-
-    function codeAddress(address) {
-        geocoder = new google.maps.Geocoder();
-        geocoder.geocode({
-            '8243 st james ave elmhurst': address
-        }, function(results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
-                var myOptions = {
-                    zoom: 8,
-                    center: results[0].geometry.location,
-                    mapTypeId: google.maps.MapTypeId.ROADMAP
-                }
-                map = new google.maps.Map(document.getElementById("googleMap"), myOptions);
-
-                var marker = new google.maps.Marker({
-                    map: map,
-                    position: results[0].geometry.location
-                });
-            }
-        });
-    }
-*/
--- sites/all/themes/wangrealestate/tpl/wre-footer.tpl.php
+++ sites/all/themes/wangrealestate/tpl/wre-footer.tpl.php
@@ -1,10 +1,21 @@
-<!-- <footer>
-				<div class="footer-pic">
-				<img src="http://placekitten.com/g/44/44">
-			</div>
-			<section class="footer-contact-container">
-				<h3 class="footer-contact">Brian Wang</h3>
-				<h3 class="footer-contact">(347)737-0158</h3>
-			</section>
-			<form class="call" action="tel:16463334444"><button class="call-button" type="submit">Call</button></form>
+<footer>
+	<div class="pic">
+		<img src="http://placekitten.com/g/44/44">
+	</div>
+	<section class="contact-container">
+		<h3 class="contact">
+			Brian Wang
+		</h3>
+		<h3 class="contact">
+			B&amp;T Realty
+		</h3>
+	</section>
+	<section class="number-container">
+		<h3 class="number first">
+			Cell: (347)737-0158
+		</h3>
+		<h3 class="number">
+			Office: (123)333-4422
+		</h3>
+	</section>
 </footer>
