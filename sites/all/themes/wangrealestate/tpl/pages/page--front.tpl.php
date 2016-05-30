084af4bcb0d71ea98454ef8d99c494ece83f05f3
Hong Jie Tan <hongjtan@gmail.com>
2016-05-30 01:53:41 -0400

    Front page updates.

--- sites/all/themes/wangrealestate/tpl/pages/page--front.tpl.php
+++ sites/all/themes/wangrealestate/tpl/pages/page--front.tpl.php
@@ -1,3 +1,18 @@
+<?php
+/**
+ * @file
+ * Template file for the front page.
+ */
+?>
+
+<?php if (!empty($wre_header)): ?>
+  <?php print render($wre_header); ?>
+<?php endif; ?>
+
 <div class="front-page">
   Front Page
 </div>
+
+<?php if (!empty($wre_footer)): ?>
+  <?php print render($wre_footer); ?>
+<?php endif; ?>
