25ab0b283f093d6a1f33bfb643079729bc757cdb
Eric Chang <szechichang@gmail.com>
2016-07-11 12:44:32 -0400

    homepage link on logo

--- sites/all/themes/wangrealestate/tpl/wre-header.tpl.php
+++ sites/all/themes/wangrealestate/tpl/wre-header.tpl.php
@@ -1,6 +1,5 @@
 <header>
   <div class="header-wrapper">
-    <h1 class="header-logo">The Real Wang</h1>
-    <h1 class="header-name">| Brian Wang</h1>
+    <h1 class="header-logo"><a href="http://www.therealwang.com">The Real Wang | Brian Wang</a></h1>
   </div>
 </header>
