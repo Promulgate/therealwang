ccf85d99117d528d5a80130922aec78c4a558ca0
Hong Jie Tan <hongjtan@gmail.com>
2016-07-13 14:17:06 +0000

    Adding meta viewport tag.

--- sites/all/themes/wangrealestate/tpl/pages/html.tpl.php
+++ sites/all/themes/wangrealestate/tpl/pages/html.tpl.php
@@ -9,6 +9,7 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
     <title>
       <?php print $head_title; ?>
     </title>
