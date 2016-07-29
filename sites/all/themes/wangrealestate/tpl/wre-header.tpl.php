f1509eec4a21132f5ccf4d5385a7a8a0e7335b30
Eric Chang <szechichang@gmail.com>
2016-07-28 20:13:03 -0400

    style change

--- sites/all/themes/wangrealestate/tpl/wre-header.tpl.php
+++ sites/all/themes/wangrealestate/tpl/wre-header.tpl.php
@@ -1,5 +1,6 @@
 <header>
   <div class="header-wrapper">
-    <h1 class="header-logo"><a href="http://www.therealwang.com">The Real Wang | Brian Wang</a></h1>
+    <h1 class="header-logo"><a href="http://www.therealwang.com">The Real Wang</a></h1>
+    <div class="header-sublogo">NYC Real Estate</div>
   </div>
 </header>
