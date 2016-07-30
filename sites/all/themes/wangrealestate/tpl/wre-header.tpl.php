c2c652ea8a5a1436d926934ff4a8ca8195a149b3
Eric Chang <szechichang@gmail.com>
2016-07-30 13:32:45 -0400

    delink logo due to home button

--- sites/all/themes/wangrealestate/tpl/wre-header.tpl.php
+++ sites/all/themes/wangrealestate/tpl/wre-header.tpl.php
@@ -9,9 +9,7 @@
   	</section>
   	<section class="header-center-cell">
 	    <h1 class="header-logo">
-	    	<a href="http://www.therealwang.com">
 	    		The Real Wang
-	    	</a>
 	    </h1>
 	    <div class="header-sublogo">
 	    	NYC Real Estate
