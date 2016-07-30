27783ec2b812420f3fb44b6bc00c22c223b5d7a3
Eric Chang <szechichang@gmail.com>
2016-07-30 13:27:35 -0400

    home button

--- sites/all/themes/wangrealestate/tpl/wre-header.tpl.php
+++ sites/all/themes/wangrealestate/tpl/wre-header.tpl.php
@@ -1,6 +1,31 @@
 <header>
-  <div class="header-wrapper">
-    <h1 class="header-logo"><a href="http://www.therealwang.com">The Real Wang</a></h1>
-    <div class="header-sublogo">NYC Real Estate</div>
-  </div>
+  <section class="header-wrapper">
+  	<section class="header-left-cell">
+  		<div class="header-home">
+  			<a class="header-home-link" href="http://www.therealwang.com">
+  				Home
+  			</a>
+  		</div>
+  	</section>
+  	<section class="header-center-cell">
+	    <h1 class="header-logo">
+	    	<a href="http://www.therealwang.com">
+	    		The Real Wang
+	    	</a>
+	    </h1>
+	    <div class="header-sublogo">
+	    	NYC Real Estate
+	    </div>
+		</section>
+		<section class="header-right-cell">
+			<!--<div class="hamburger">
+			</div>
+			<div class="hamburger-menu">
+					<ul class="hamburger-list">
+						<li class="hamburger-list-content hamburger-border">About</li>
+						<li class="hamburger-list-content">Contact</li>
+					</ul>
+				</div>-->
+		</section>
+  </section>
 </header>
