63d8c4264cd1ee1f101635d2e36c8898aa7df651
Eric Chang <szechichang@gmail.com>
2016-08-04 16:58:55 -0400

    responsive header fix

--- sites/all/themes/wangrealestate/tpl/wre-header.tpl.php
+++ sites/all/themes/wangrealestate/tpl/wre-header.tpl.php
@@ -1,29 +1,15 @@
 <header>
   <section class="header-wrapper">
-  	<section class="header-left-cell">
-  		<div class="header-home">
-  			<a class="header-home-link" href="http://www.therealwang.com">
-  				Home
-  			</a>
-  		</div>
-  	</section>
-  	<section class="header-center-cell">
+    <section class="header-center-cell">
+			<a class="header-home-link" href="http://www.therealwang.com">
+				Home
+			</a>
 	    <h1 class="header-logo">
 	    		The Real Wang
 	    </h1>
 	    <div class="header-sublogo">
 	    	NYC Real Estate
 	    </div>
-		</section>
-		<section class="header-right-cell">
-			<!--<div class="hamburger">
-			</div>
-			<div class="hamburger-menu">
-					<ul class="hamburger-list">
-						<li class="hamburger-list-content hamburger-border">About</li>
-						<li class="hamburger-list-content">Contact</li>
-					</ul>
-				</div>-->
 		</section>
   </section>
 </header>
